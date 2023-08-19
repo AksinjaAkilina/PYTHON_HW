1892acecfe1b28e040b970b901e89f10ee3aee78
Akilina Ksenia <ksusysol@gmail.com>
2023-08-19 14:46:12 +0300

    добавлено решение для дз 8

--- HW_8/run.py
+++
@@ -1,9 +0,0 @@
-from user import choose_mode
-from filling_dictionary import filling
-
-def running():
-    person, mode = choose_mode()
-    if mode == 'запись':
-        filling(person)
-    else:
-        pass
